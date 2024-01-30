ff886202093b4f9838fe688f07f4bc4aae6db514
Elizabethola <hellomojisolalizzy@gmail.com>
2024-01-29 16:25:27 -0800

    updated

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -2,23 +2,17 @@
 
 
 /**
- * main - program that prints the alphabet in lowercase and upper case
+ * main - program that prints the alphabet in lowercase
  *
- * Return: Always 0.
+ *  Return: Always 0.
  */
-
 int main(void)
 {
 	char letter;
 
 	for (letter = 'a'; letter <= 'z'; letter++)
-
-	{
-	if (letter != 'e' && letter != 'q')
 	putchar(letter);
-	}
-
 	putchar('\n');
-
 	return (0);
 }
+
